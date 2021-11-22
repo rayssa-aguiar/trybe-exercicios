5d886d6074769eb8f02ab4f5419a9690af382b11
Camila Oliveira <rayssacamila77@gmail.com>
2021-11-22 11:59:50 -0300

    modificando elementos utilizando funcoes DOM

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName('p')[0].innerText = 'Como você se vê daqui a 2 anos?';
+document.getElementsByTagName('p')[1].innerText = 'Formada em desenvolvimento web e trabalhando na área.';
+document.querySelector('.main-content').style.backgroundColor = 'rgb(76,164,109)';
+document.querySelector('.center-content').style.backgroundColor = 'white';
+document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - JavaScript';
+document.querySelectorAll('p')[1].style.textTransform = 'uppercase';
