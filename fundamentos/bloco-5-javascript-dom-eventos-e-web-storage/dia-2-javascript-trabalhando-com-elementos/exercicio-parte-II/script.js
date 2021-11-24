393ac64cb9656f29a971cc7c75ed6e50d9d7faec
Camila Oliveira <rayssacamila77@gmail.com>
2021-11-24 09:15:12 -0300

    primeiro commit

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio-parte-II/script.js
@@ -0,0 +1,11 @@
+// parentNode : retorna o nó pai.
+// parentElement : retorna o elemento pai.
+// childNodes : retorna um NodeList com todos os nós filhos.
+// children : retorna um HTMLCollection com todos os elementos filhos.
+// firstChild : retorna o primeiro nó filho.
+// firstElementChild : retorna o primeiro elemento filho.
+// lastChild : retorna o último nó filho.
+// lastElementChild : retorna o último elemento filho.
+// nextSibling : retorna o próximo nó.
+//previousElementSibling: A propriedade previousElementSibling retorna o elemento anterior do elemento especificado, no mesmo nível de árvore.
+
